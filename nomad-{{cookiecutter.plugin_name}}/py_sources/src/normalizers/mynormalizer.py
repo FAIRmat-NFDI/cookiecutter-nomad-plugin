b3c9021a31d1a8c29c81750c0b963638675017bf
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2024-04-29 12:23:18 +0200

    refactor: update normalizer interface

--- nomad-{{cookiecutter.plugin_name}}/py_sources/src/normalizers/mynormalizer.py
+++ nomad-{{cookiecutter.plugin_name}}/py_sources/src/normalizers/mynormalizer.py
@@ -1,12 +1,9 @@
 from nomad.datamodel.metainfo.workflow import Workflow
 from nomad.normalizing import Normalizer
 
-
 class MyNormalizer(Normalizer):
-    domain = None
 
-    def normalize(self, logger):
+    def normalize(self, archive, logger):
         super().normalize(logger)
         logger.info('ExampleNormalizer called')
-
-        self.entry_archive.workflow2 = Workflow(name='Example workflow')
+        archive.workflow2 = Workflow(name='Example workflow')
