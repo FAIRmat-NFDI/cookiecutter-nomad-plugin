a0794e3666010390f710f5774e6e367372e1f5a4
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2024-05-14 15:09:48 +0200

    chore: use structlog and typing

--- nomad-{{cookiecutter.plugin_name}}/py_sources/src/parsers/myparser.py
+++ nomad-{{cookiecutter.plugin_name}}/py_sources/src/parsers/myparser.py
@@ -1,18 +1,31 @@
+from typing import (
+    TYPE_CHECKING,
+)
+
+if TYPE_CHECKING:
+    from nomad.datamodel.datamodel import (
+        EntryArchive,
+    )
+    from structlog.stdlib import (
+        BoundLogger,
+    )
+
 from nomad.config import config
-from nomad.datamodel import EntryArchive
 from nomad.datamodel.results import Material, Results
 from nomad.parsing.parser import MatchingParser
 
-configuration = config.get_plugin_entry_point('nomad_{{cookiecutter.module_name}}.parsers:myparser')
+configuration = config.get_plugin_entry_point(
+    'nomad_{{cookiecutter.module_name}}.parsers:myparser'
+)
 
 
 class MyParser(MatchingParser):
     def parse(
         self,
         mainfile: str,
-        archive: EntryArchive,
-        logger=None,
-        child_archives: dict[str, EntryArchive] = None,
+        archive: 'EntryArchive',
+        logger: 'BoundLogger',
+        child_archives: dict[str, 'EntryArchive'] = None,
     ) -> None:
         logger.info(f'MyParser.parse: parameter={configuration.parameter}')
         archive.results = Results(material=Material(elements=['H', 'O']))
--- nomad-{{cookiecutter.plugin_name}}/py_sources/src/schema_packages/mypackage.py
+++ nomad-{{cookiecutter.plugin_name}}/py_sources/src/schema_packages/mypackage.py
@@ -1,9 +1,23 @@
+from typing import (
+    TYPE_CHECKING,
+)
+
+if TYPE_CHECKING:
+    from nomad.datamodel.datamodel import (
+        EntryArchive,
+    )
+    from structlog.stdlib import (
+        BoundLogger,
+    )
+
 from nomad.config import config
 from nomad.datamodel.data import Schema
 from nomad.datamodel.metainfo.annotations import ELNAnnotation, ELNComponentEnum
 from nomad.metainfo import Quantity, SchemaPackage
 
-configuration = config.get_plugin_entry_point('nomad_{{cookiecutter.module_name}}.schema_packages:mypackage')
+configuration = config.get_plugin_entry_point(
+    'nomad_{{cookiecutter.module_name}}.schema_packages:mypackage'
+)
 
 m_package = SchemaPackage()
 
@@ -14,11 +28,11 @@ class MySchema(Schema):
     )
     message = Quantity(type=str)
 
-    def normalize(self, archive, logger):
+    def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
         super().normalize(archive, logger)
 
         logger.info(f'MySchema.normalize: parameter={configuration.parameter}')
         self.message = f'Hello {self.name}!'
 
- 
+
 m_package.__init_metainfo__()
