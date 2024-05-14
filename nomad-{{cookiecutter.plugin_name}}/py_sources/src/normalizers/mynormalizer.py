eeaed54fa2010c0cd5ca0e307e53ecb8f86619df
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2024-05-14 15:09:48 +0200

    chore: use structlog and typing

--- nomad-{{cookiecutter.plugin_name}}/py_sources/src/normalizers/mynormalizer.py
+++ nomad-{{cookiecutter.plugin_name}}/py_sources/src/normalizers/mynormalizer.py
@@ -1,11 +1,25 @@
+from typing import (
+    TYPE_CHECKING,
+)
+
+if TYPE_CHECKING:
+    from nomad.datamodel.datamodel import (
+        EntryArchive,
+    )
+    from structlog.stdlib import (
+        BoundLogger,
+    )
+
 from nomad.config import config
 from nomad.normalizing import Normalizer
 
-configuration = config.get_plugin_entry_point('nomad_{{cookiecutter.module_name}}.normalizers:mynormalizer')
+configuration = config.get_plugin_entry_point(
+    'nomad_{{cookiecutter.module_name}}.normalizers:mynormalizer'
+)
 
 
 class MyNormalizer(Normalizer):
-    def normalize(self, archive, logger):
+    def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
         super().normalize(logger)
         logger.info(f'MyNormalizer.normalize: parameter={configuration.parameter}')
         if archive.results and archive.results.material:
