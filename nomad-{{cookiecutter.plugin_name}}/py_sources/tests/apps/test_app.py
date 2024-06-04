d5473ceb5c7f0dc42b975b139551484acd8aceff
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2024-06-04 12:46:28 +0200

    refactor: add a test for apps

--- nomad-{{cookiecutter.plugin_name}}/py_sources/tests/apps/test_app.py
+++ nomad-{{cookiecutter.plugin_name}}/py_sources/tests/apps/test_app.py
@@ -2,3 +2,5 @@ def test_importing_app():
     # this will raise an exception if pydantic model validation fails for th app
     from nomad_{{cookiecutter.module_name}}.apps import myapp
 
+    assert myapp.app.label == 'MyApp'
+
