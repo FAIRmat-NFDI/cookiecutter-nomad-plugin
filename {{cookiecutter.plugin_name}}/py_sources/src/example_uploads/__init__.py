158b98129ce1b5e7fd6d79e9ed300fcf64960285
JosePizarro3 <jose.pizarro@physik.hu-berlin.de>
2024-08-23 14:13:41 +0200

    Changed exampleupload entry point

--- {{cookiecutter.plugin_name}}/py_sources/src/example_uploads/__init__.py
+++ {{cookiecutter.plugin_name}}/py_sources/src/example_uploads/__init__.py
@@ -1,7 +1,7 @@
 from nomad.config.models.plugins import ExampleUploadEntryPoint
 
-myexampleupload = ExampleUploadEntryPoint(
-    title='My Example Upload',
+example_upload_entry_point = ExampleUploadEntryPoint(
+    title='New Example Upload',
     category='Examples',
     description='Description of this example upload.',
     path='example_uploads/getting_started',
