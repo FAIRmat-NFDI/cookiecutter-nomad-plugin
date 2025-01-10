74550fcbd23d262a1ee55e45c0e85a6e7b879c3c
Lauri Himanen <lauri.himanen@gmail.com>
2025-01-10 10:09:43 +0200

    Fixed linting.

--- {{cookiecutter.plugin_name}}/py_sources/src/example_uploads/__init__.py
+++ {{cookiecutter.plugin_name}}/py_sources/src/example_uploads/__init__.py
@@ -3,8 +3,6 @@ from nomad.config.models.plugins import ExampleUploadEntryPoint
 example_upload_entry_point = ExampleUploadEntryPoint(
     title='New Example Upload',
     category='Examples',
-    description='This example upload contains the contents of the getting_started folder.',
-    resources=[
-	    'example_uploads/getting_started/*',
-	]
+    description='Contains the contents of the getting_started folder.',
+    resources=['example_uploads/getting_started/*']
 )
