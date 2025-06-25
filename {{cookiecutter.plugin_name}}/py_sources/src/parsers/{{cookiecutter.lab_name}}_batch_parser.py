4f8f3f5182e48a4809790325e3470a19b316b2b1
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2025-06-25 12:02:29 +0200

    copy files over

---
+++ {{cookiecutter.plugin_name}}/py_sources/src/parsers/{{cookiecutter.lab_name}}_batch_parser.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+"""
+Created on Fri Sep 27 09:08:03 2024
+
+@author: a2853
+"""
+
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import pandas as pd
+from baseclasses.helper.solar_cell_batch_mapping import (
+    get_reference,
+    map_atomic_layer_deposition,
+    map_basic_sample,
+    map_batch,
+    map_cleaning,
+    map_evaporation,
+    map_generic,
+    map_inkjet_printing,
+    map_laser_scribing,
+    map_sdc,
+    map_spin_coating,
+    map_sputtering,
+    map_substrate,
+)
+from baseclasses.helper.utilities import create_archive
+from nomad.datamodel import EntryArchive
+from nomad.datamodel.data import EntryData
+from nomad.datamodel.metainfo.basesections import Entity
+from nomad.metainfo import Quantity
+from nomad.parsing import MatchingParser
+
+from nomad_hysprint.parsers.file_parser.ink_recycling_mappers import map_ink_recycling
+from nomad_hysprint.schema_packages.hysprint_package import (
+    HySprint_Batch,
+    HySprint_Cleaning,
+    HySprint_Evaporation,
+    HySprint_Inkjet_Printing,
+    HySprint_LaserScribing,
+    HySprint_Process,
+    HySprint_Sample,
+    HySprint_SlotDieCoating,
+    HySprint_SpinCoating,
+    HySprint_Sputtering,
+    HySprint_Substrate,
+    IRIS_AtomicLayerDeposition,
+    ProcessParameter,
+)
+from nomad_hysprint.schema_packages.ink_recycling_package import (
+    InkRecycling_RecyclingExperiment,  # Added back this import
+)
+
+"""
+This is a hello world style example for an example parser/converter.
+"""
+
+
+class RawHySprintExperiment(EntryData):
+    processed_archive = Quantity(type=Entity, shape=['*'])
+
+
+def map_generic_parameters(process, data):
+    parameters = []
+    for col, val in data.items():
+        if col in ['Notes', 'Name']:
+            continue
+        if pd.isna(val):
+            continue
+        try:
+            val_float = float(val)
+            parameters.append(ProcessParameter(name=col, value_number=val_float))
+        except Exception:
+            parameters.append(ProcessParameter(name=col, value_string=val))
+    process.process_parameters = parameters
+
+
+class HySprintExperimentParser(MatchingParser):
+    def is_mainfile(
+        self,
+        filename: str,
+        mime: str,
+        buffer: bytes,
+        decoded_buffer: str,
+        compression: str = None,
+    ):
+        is_mainfile_super = super().is_mainfile(filename, mime, buffer, decoded_buffer, compression)
+        if not is_mainfile_super:
+            return False
+        try:
+            df = pd.read_excel(filename, header=[0, 1])
+            df['Experiment Info']['Nomad ID'].dropna().to_list()
+        except Exception:
+            return False
+        return True
+
+    def parse(self, mainfile: str, archive: EntryArchive, logger):
+        upload_id = archive.metadata.upload_id
+        # xls = pd.ExcelFile(mainfile)
+        df = pd.read_excel(mainfile, header=[0, 1])
+
+        sample_ids = df['Experiment Info']['Nomad ID'].dropna().to_list()
+        batch_id = '_'.join(sample_ids[0].split('_')[:-1])
+        archives = [map_batch(sample_ids, batch_id, upload_id, HySprint_Batch)]
+        substrates = []
+        substrates_col = [
+            'Sample dimension',
+            'Sample area [cm^2]',
+            'Pixel area [cm^2]',
+            'Number of pixels',
+            'Notes',
+            'Substrate material',
+            'Substrate conductive layer',
+        ]
+        substrates_col = [s for s in substrates_col if s in df['Experiment Info'].columns]
+        for i, sub in df['Experiment Info'][substrates_col].drop_duplicates().iterrows():
+            if pd.isna(sub).all():
+                continue
+            substrates.append((f'{i}_substrate', sub, map_substrate(sub, HySprint_Substrate)))
+
+        def find_substrate(d):
+            for s in substrates:
+                if d.equals(s[1]):
+                    return s[0]
+
+        for i, row in df['Experiment Info'].iterrows():
+            if pd.isna(row).all():
+                continue
+            substrate_name = find_substrate(row[substrates_col]) + '.archive.json' if substrates_col else None
+            archives.append(map_basic_sample(row, substrate_name, upload_id, HySprint_Sample))
+
+        for i, col in enumerate(df.columns.get_level_values(0).unique()):
+            if col == 'Experiment Info':
+                continue
+
+            df_dropped = df[col].drop_duplicates()
+            for j, row in df_dropped.iterrows():
+                if row.dropna().empty:
+                    continue
+                lab_ids = [
+                    x['Experiment Info']['Nomad ID']
+                    for _, x in df[['Experiment Info', col]].iterrows()
+                    if x[col].astype('object').equals(row.astype('object'))
+                ]
+                if 'Cleaning' in col:
+                    archives.append(map_cleaning(i, j, lab_ids, row, upload_id, HySprint_Cleaning))
+
+                if 'Laser Scribing' in col:
+                    archives.append(map_laser_scribing(i, j, lab_ids, row, upload_id, HySprint_LaserScribing))
+
+                if 'Ink Recycling' in col:
+                    archives.append(
+                        map_ink_recycling(i, j, lab_ids, row, upload_id, InkRecycling_RecyclingExperiment)
+                    )
+
+                if 'Generic Process' in col:  # move up
+                    generic_process = map_generic(i, j, lab_ids, row, upload_id, HySprint_Process)
+                    map_generic_parameters(generic_process[1], row)
+                    archives.append(generic_process)
+
+                if pd.isna(row.get('Material name')):
+                    continue
+
+                if 'Evaporation' in col:
+                    coevap = False
+                    if 'Co-Evaporation' in col:
+                        coevap = True
+                    archives.append(
+                        map_evaporation(i, j, lab_ids, row, upload_id, HySprint_Evaporation, coevap)
+                    )
+
+                if 'Spin Coating' in col:
+                    archives.append(map_spin_coating(i, j, lab_ids, row, upload_id, HySprint_SpinCoating))
+
+                if 'Slot Die Coating' in col:
+                    archives.append(map_sdc(i, j, lab_ids, row, upload_id, HySprint_SlotDieCoating))
+
+                if 'Sputtering' in col:
+                    archives.append(map_sputtering(i, j, lab_ids, row, upload_id, HySprint_Sputtering))
+
+                if 'Inkjet Printing' in col:
+                    archives.append(
+                        map_inkjet_printing(i, j, lab_ids, row, upload_id, HySprint_Inkjet_Printing)
+                    )
+
+                if 'ALD' in col:
+                    archives.append(
+                        map_atomic_layer_deposition(i, j, lab_ids, row, upload_id, IRIS_AtomicLayerDeposition)
+                    )
+
+        refs = []
+        for subs in substrates:
+            file_name = f'{subs[0]}.archive.json'
+            create_archive(subs[2], archive, file_name)
+            refs.append(get_reference(upload_id, file_name))
+
+        for a in archives:
+            file_name = f'{a[0]}.archive.json'
+            create_archive(a[1], archive, file_name)
+            refs.append(get_reference(upload_id, file_name))
+
+        archive.data = RawHySprintExperiment(processed_archive=refs)
---
+++ {{cookiecutter.plugin_name}}/py_sources/src/parsers/{{cookiecutter.lab_name}}_measurement_parser.py
@@ -0,0 +1,183 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import datetime
+import os
+import sys
+
+from baseclasses.helper.utilities import (
+    create_archive,
+    get_entry_id_from_file_name,
+    get_reference,
+    set_sample_reference,
+)
+from nomad.datamodel import EntryArchive
+from nomad.datamodel.data import (
+    EntryData,
+)
+from nomad.datamodel.metainfo.basesections import (
+    Activity,
+)
+from nomad.metainfo import (
+    Quantity,
+)
+from nomad.parsing import MatchingParser
+
+from nomad_hysprint.schema_packages.hysprint_package import (
+    HySprint_CyclicVoltammetry,
+    HySprint_ElectrochemicalImpedanceSpectroscopy,
+    HySprint_EQEmeasurement,
+    HySprint_JVmeasurement,
+    HySprint_Measurement,
+    HySprint_OpenCircuitVoltage,
+    HySprint_PLImaging,
+    HySprint_PLmeasurement,
+    HySprint_SEM,
+    HySprint_SimpleMPPTracking,
+    HySprint_trSPVmeasurement,
+    HySprint_UVvismeasurement,
+    HySprint_XPS,
+    HySprint_XRD_XY,
+    HZB_EnvironmentMeasurement,
+    HZB_NKData,
+)
+
+"""
+This is a hello world style example for an example parser/converter.
+"""
+
+
+class RawFileHZB(EntryData):
+    processed_archive = Quantity(
+        type=Activity,
+    )
+
+
+def update_general_process_entries(entry, entry_id, archive, logger):
+    from nomad import files
+    from nomad.search import search
+
+    query = {
+        'entry_id': entry_id,
+    }
+    search_result = search(owner='all', query=query, user_id=archive.metadata.main_author.user_id)
+    entry_type = search_result.data[0].get('entry_type') if len(search_result.data) == 1 else None
+    if entry_type != 'HySprint_Measurement':
+        return None
+    new_entry_dict = entry.m_to_dict()
+    res = search_result.data[0]
+    try:
+        # Open Archives
+        with files.UploadFiles.get(upload_id=res['upload_id']).read_archive(entry_id=res['entry_id']) as ar:
+            entry_id = res['entry_id']
+            entry_data = ar[entry_id]['data']
+            entry_data.pop('m_def', None)
+            new_entry_dict.update(entry_data)
+    except Exception:
+        pass
+        # logger.error('Error in processing data: ', e)
+    print(type(entry).__name__)
+    new_entry = getattr(sys.modules[__name__], type(entry).__name__).m_from_dict(new_entry_dict)
+    return new_entry
+
+
+class HySprintParser(MatchingParser):
+    def parse(self, mainfile: str, archive: EntryArchive, logger):
+        # Log a hello world, just to get us started. TODO remove from an actual parser.
+
+        mainfile_split = os.path.basename(mainfile).split('.')
+        notes = ''
+        if len(mainfile_split) > 2:
+            notes = '.'.join(mainfile_split[1:-2])
+        measurment_type = mainfile_split[-2].lower()
+        entry = HySprint_Measurement()
+        if mainfile_split[-1] == 'mpt' and measurment_type == 'hy':
+            from nomad_hysprint.schema_packages.file_parser.mps_file_parser import (
+                read_mpt_file,
+            )
+
+            with open(mainfile) as f:
+                metadata, _, technique = read_mpt_file(f)
+            if 'Cyclic Voltammetry' in technique:
+                entry = HySprint_CyclicVoltammetry()
+            if 'Open Circuit Voltage' in technique:
+                entry = HySprint_OpenCircuitVoltage()
+            if 'Potentio Electrochemical Impedance Spectroscopy' in technique:
+                entry = HySprint_ElectrochemicalImpedanceSpectroscopy()
+        if mainfile_split[-1] == 'csv' and measurment_type == 'hy':
+            with open(mainfile) as f:
+                file_content = f.read()
+            if (
+                'Experiment:' in file_content
+                and 'Start date:' in file_content
+                and ' Charge (C)' in file_content
+            ):
+                entry = HySprint_CyclicVoltammetry()
+            if (
+                'Experiment:' in file_content
+                and 'Start date:' in file_content
+                and 'Aux A (V)' in file_content
+            ):
+                entry = HySprint_OpenCircuitVoltage()
+        if mainfile_split[-1] == 'txt' and measurment_type == 'jv':
+            entry = HySprint_JVmeasurement()
+        if mainfile_split[-1] == 'txt' and measurment_type == 'spv':
+            entry = HySprint_trSPVmeasurement()
+        if (mainfile_split[-1] == 'txt' or mainfile_split[-1] == 'TRQ') and measurment_type == 'eqe':
+            entry = HySprint_EQEmeasurement()
+        if mainfile_split[-1] in ['tif', 'tiff'] and measurment_type.lower() == 'sem':
+            entry = HySprint_SEM()
+            entry.detector_data = [os.path.basename(mainfile)]
+        if measurment_type == 'pl':
+            entry = HySprint_PLmeasurement()
+        if measurment_type == 'pli':
+            entry = HySprint_PLImaging()
+        if measurment_type == 'xrd' and mainfile_split[-1] == 'xy':
+            entry = HySprint_XRD_XY()
+        if measurment_type == 'xps' and mainfile_split[-1] in ['xy']:
+            entry = HySprint_XPS()
+        if measurment_type == 'uvvis':
+            entry = HySprint_UVvismeasurement()
+            entry.data_file = [os.path.basename(mainfile)]
+        if mainfile_split[-1] in ['txt'] and measurment_type == 'env':
+            entry = HZB_EnvironmentMeasurement()
+        if mainfile_split[-1] in ['nk']:
+            entry = HZB_NKData()
+        if mainfile_split[-1] in ['txt', 'csv'] and measurment_type == 'mppt':
+            entry = HySprint_SimpleMPPTracking()
+        archive.metadata.entry_name = os.path.basename(mainfile)
+
+        if mainfile_split[-1] not in ['nk']:
+            search_id = mainfile_split[0]
+            set_sample_reference(archive, entry, search_id)
+
+            entry.name = f'{search_id} {notes}'
+            entry.description = f'Notes from file name: {notes}'
+
+        if measurment_type not in ['uvvis', 'sem', 'SEM']:
+            entry.data_file = os.path.basename(mainfile)
+        entry.datetime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+
+        file_name = f'{os.path.basename(mainfile)}.archive.json'
+        eid = get_entry_id_from_file_name(file_name, archive)
+        archive.data = RawFileHZB(processed_archive=get_reference(archive.metadata.upload_id, eid))
+        new_entry_created = create_archive(entry, archive, file_name)
+        if not new_entry_created:
+            new_entry = update_general_process_entries(entry, eid, archive, logger)
+            if new_entry is not None:
+                create_archive(new_entry, archive, file_name, overwrite=True)
