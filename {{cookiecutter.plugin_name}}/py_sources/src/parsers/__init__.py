56bef49a4322ad3fc09b77eea64498bffde19d0c
Haoyu (Daniel) YANG 杨浩宇 <yanghaoyu97@outlook.com>
2025-06-26 16:24:15 +0200

    Add entry points (#2)
    * add entry points for parsers
    
    * add entry point for schema package
    
    * reorganize entry points registration

--- {{cookiecutter.plugin_name}}/py_sources/src/parsers/__init__.py
+++ {{cookiecutter.plugin_name}}/py_sources/src/parsers/__init__.py
@@ -11,8 +11,40 @@ class NewParserEntryPoint(ParserEntryPoint):
         return NewParser(**self.model_dump())
 
 
+class {{cookiecutter.lab_name}}ExperimentParserEntryPoint(ParserEntryPoint):
+    parameter: int = Field(0, description='Custom configuration parameter')
+
+    def load(self):
+        from {{cookiecutter.module_name}}.parsers.{{cookiecutter.lab_name}}_batch_parser import {{cookiecutter.lab_name}}ExperimentParser
+
+        return {{cookiecutter.lab_name}}ExperimentParser(**self.model_dump())
+
+
+class {{cookiecutter.lab_name}}ParserEntryPoint(ParserEntryPoint):
+    parameter: int = Field(0, description='Custom configuration parameter')
+
+    def load(self):
+        from {{cookiecutter.module_name}}.parsers.{{cookiecutter.lab_name}}_measurement_parser import {{cookiecutter.lab_name}}Parser
+
+        return {{cookiecutter.lab_name}}Parser(**self.model_dump())
+
+
 parser_entry_point = NewParserEntryPoint(
     name='NewParser',
     description='New parser entry point configuration.',
+    mainfile_name_re=r'.*\.newmainfilename',
+)
+
+
+{{cookiecutter.lab_name}}_experiment_parser_entry_point = {{cookiecutter.lab_name}}ExperimentParserEntryPoint(
+    name='{{cookiecutter.lab_name}}ExperimentParserEntryPoint',
+    description='{{cookiecutter.lab_name}} experiment parser entry point configuration.',
+    mainfile_name_re=r'.*\.newmainfilename',
+)
+
+
+{{cookiecutter.lab_name}}_parser_entry_point = {{cookiecutter.lab_name}}ParserEntryPoint(
+    name='{{cookiecutter.lab_name}}ParserEntryPoint',
+    description='{{cookiecutter.lab_name}} parser entry point configuration.',
     mainfile_name_re=r'.*\.newmainfilename',
 )
--- {{cookiecutter.plugin_name}}/py_sources/src/schema_packages/__init__.py
+++ {{cookiecutter.plugin_name}}/py_sources/src/schema_packages/__init__.py
@@ -11,7 +11,22 @@ class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
         return m_package
 
 
+class {{cookiecutter.lab_name}}PackageEntryPoint(SchemaPackageEntryPoint):
+    parameter: int = Field(0, description='Custom configuration parameter')
+
+    def load(self):
+        from {{cookiecutter.module_name}}.schema_packages.{{cookiecutter.lab_name}}_package import m_package
+
+        return m_package
+
+
 schema_package_entry_point = NewSchemaPackageEntryPoint(
     name='NewSchemaPackage',
     description='New schema package entry point configuration.',
+)
+
+
+{{cookiecutter.lab_name}}_schema_package_entry_point = {{cookiecutter.lab_name}}PackageEntryPoint(
+    name='{{cookiecutter.lab_name}}Package',
+    description='{{cookiecutter.lab_name}} package entry point configuration.',
 )
