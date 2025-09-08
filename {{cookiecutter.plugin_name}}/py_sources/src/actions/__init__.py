5a1f7da4627d52c67e2ae0b80fa25d473aa1c210
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2025-09-08 12:34:41 +0200

    Add action generations to template

---
+++ {{cookiecutter.plugin_name}}/py_sources/src/actions/__init__.py
@@ -0,0 +1,26 @@
+from nomad.config.models.plugins import ActionEntryPoint
+from nomad.actions import TaskQueue
+from pydantic import Field
+
+
+class SimpleActionEntryPoint(ActionEntryPoint):
+    task_queue: str = Field(
+        default=TaskQueue.CPU, description='Determines the task queue for this action'
+    )
+
+    def load(self):
+        from nomad.actions import Action
+        from {{cookiecutter.module_name}}.actions.simple_action.workflows import SimpleWorkflow
+        from {{cookiecutter.module_name}}.actions.simple_action.activities import greet
+
+        return Action(
+            task_queue=self.task_queue,
+            workflow=SimpleWorkflow,
+            activities=[greet],
+        )
+
+
+simple_action = SimpleActionEntryPoint(
+    name='SimpleAction',
+    description='A simple action that returns a greeting.',
+)
