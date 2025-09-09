dd54dc7ab517915ed99d5fe061cc15348be86427
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2025-09-09 11:03:10 +0200

    Move entrypoint to subfolder

--- {{cookiecutter.plugin_name}}/py_sources/src/actions/__init__.py
+++ {{cookiecutter.plugin_name}}/py_sources/src/actions/__init__.py
@@ -1,28 +1,3 @@
-from nomad.actions import TaskQueue
-from pydantic import Field
-from temporalio import workflow
+from {{cookiecutter.module_name}}.actions.simple_action import simple_action_entry_point
 
-with workflow.unsafe.imports_passed_through():
-    from nomad.config.models.plugins import ActionEntryPoint
-
-class SimpleActionEntryPoint(ActionEntryPoint):
-    task_queue: str = Field(
-        default=TaskQueue.CPU, description='Determines the task queue for this action'
-    )
-
-    def load(self):
-        from nomad.actions import Action
-        from {{cookiecutter.module_name}}.actions.simple_action.workflows import SimpleWorkflow
-        from {{cookiecutter.module_name}}.actions.simple_action.activities import greet
-
-        return Action(
-            task_queue=self.task_queue,
-            workflow=SimpleWorkflow,
-            activities=[greet],
-        )
-
-
-simple_action = SimpleActionEntryPoint(
-    name='SimpleAction',
-    description='A simple action that returns a greeting.',
-)
+__all__ = ["simple_action_entry_point"]
