295ec50dd48ddc8c752b7b584bfaad01f4edf2f4
Rubel <rubel.mozumder@outlook.com>
2026-01-21 11:41:21 +0100

    Get app test back.

--- {{cookiecutter.plugin_name}}/py_sources/tests/apps/test_app.py
+++ {{cookiecutter.plugin_name}}/py_sources/tests/apps/test_app.py
@@ -1,7 +1,5 @@
-def test_importing_north_tool():
-    # this will raise an exception if pydantic model validation fails for an app
-    from {{cookiecutter.module_name}}.north_tools.{{ cookiecutter.module_name }}_jupyter import north_tool_jupyter
+def test_importing_app():
+    # this will raise an exception if pydantic model validation fails for th app
+    from {{cookiecutter.module_name}}.apps import app_entry_point
 
-    assert (north_tool_jupyter.id_url_safe == "nomad-north-{{ cookiecutter.plugin_name }}" or 
-            north_tool_jupyter.id == "nomad-north-{{ cookiecutter.plugin_name }}"), \
-        "North tool entry point has incorrect id or id_url_safe"
+    assert app_entry_point.app.label == 'NewApp'
