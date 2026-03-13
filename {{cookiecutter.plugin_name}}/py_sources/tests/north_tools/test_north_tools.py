dabfdbc7c88a9eb56b08892139c66fcb0c840b4f
Rubel <rubel.mozumder@outlook.com>
2026-03-13 09:36:58 +0100

    Shorter name for entry point.

--- {{cookiecutter.plugin_name}}/py_sources/tests/north_tools/test_north_tools.py
+++ {{cookiecutter.plugin_name}}/py_sources/tests/north_tools/test_north_tools.py
@@ -1,10 +1,10 @@
 def test_importing_north_tool():
     # this will raise an exception if pydantic model validation fails for the north tool
     from {{cookiecutter.module_name}}.north_tools import (
-        north_tool_entry_point,
+        north_tool,
     )
 
     assert (
-        north_tool_entry_point.id_url_safe == '{{cookiecutter.module_name | replace("_", "-")}}-{{cookiecutter.north_tool_name | replace("_", "-")}}'
-        or north_tool_entry_point.id == 'nomad-north-{{cookiecutter.plugin_name}}'
+        north_tool.id_url_safe == '{{cookiecutter.module_name | replace("_", "-")}}-{{cookiecutter.north_tool_name | replace("_", "-")}}'
+        or north_tool.id == 'nomad-north-{{cookiecutter.plugin_name}}'
     ), 'NORTHtool entry point has incorrect id or id_url_safe'
