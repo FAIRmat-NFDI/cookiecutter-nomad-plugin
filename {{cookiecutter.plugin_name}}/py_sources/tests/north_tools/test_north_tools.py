af067cead8b71f3e07d3a650d9aaa6b9198cbd08
Rubel <rubel.mozumder@outlook.com>
2026-03-16 09:38:24 +0100

    Make the north_tool_entry_pont shorter.

--- {{cookiecutter.plugin_name}}/py_sources/tests/north_tools/test_north_tools.py
+++ {{cookiecutter.plugin_name}}/py_sources/tests/north_tools/test_north_tools.py
@@ -1,9 +1,9 @@
 def test_importing_north_tool():
     # this will raise an exception if pydantic model validation fails
-    from {{cookiecutter.module_name}}.north_tools import north_tool_entry_point
+    from {{cookiecutter.module_name}}.north_tools import north_entry_point
 
     expected_id = '{{cookiecutter.module_name | replace("_", "-")}}-{{cookiecutter.north_tool_name | replace("_", "-")}}'
     assert (
-        north_tool_entry_point.id_url_safe == expected_id
-        or north_tool_entry_point.id == 'nomad-north-{{cookiecutter.plugin_name}}'
+        north_entry_point.id_url_safe == expected_id
+        or north_entry_point.id == 'nomad-north-{{cookiecutter.plugin_name}}'
     ), 'NORTHTool entry point has incorrect id or id_url_safe'
